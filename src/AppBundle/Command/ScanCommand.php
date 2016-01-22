92c69dbe347644cef582842416217af10c7cfa18
Benjamin Kleiner <bizzl@users.sourceforge.net>
2016-01-22 21:23:43 +0100

    Added notifications

--- src/AppBundle/Command/ScanCommand.php
+++ src/AppBundle/Command/ScanCommand.php
@@ -4,6 +4,7 @@ namespace AppBundle\Command;
 
 
 use AppBundle\Entity\Download;
+use AppBundle\Entity\Notification;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -14,6 +15,8 @@ use Symfony\Component\Process\ProcessBuilder;
 
 class ScanCommand extends ContainerAwareCommand
 {
+    private $pathes = [];
+
     protected function configure()
     {
         $this
@@ -32,6 +35,7 @@ class ScanCommand extends ContainerAwareCommand
             return -1;
         }
 
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $url = $input->getArgument('url');
         if ($url) {
             $referer = $input->getArgument('referer');
@@ -41,13 +45,12 @@ class ScanCommand extends ContainerAwareCommand
             $downloads = $this->scan($url, $referer, $output);
             if ($downloads) {
                 $results = $this->saveDownloads($downloads);
-                $output->writeln("{$results['saved']} saved, {$results['skipped']} skipped");
+                $this->successNotification($results, $url, $referer);
             } else {
-                $output->writeln("Failed to scan {$url} (Referer: {$referer})");
+                $this->failureNotification($url, $referer);
             }
         } else {
             $count = $input->getOption('count');
-            $em = $this->getContainer()->get('doctrine.orm.entity_manager');
             $repo = $em->getRepository('AppBundle:QueuedUrl');
             $ids = $repo
                 ->createQueryBuilder('q')
@@ -61,17 +64,17 @@ class ScanCommand extends ContainerAwareCommand
                 $queuedUrl = $repo->find($id['id']);
                 $url = $queuedUrl->getUrl();
                 $referer = $queuedUrl->getReferer();
-                $downloads = $this->scan($url, $referer, $output);
+                $downloads = $this->scan($url, $referer);
                 if ($downloads) {
                     $results = $this->saveDownloads($downloads);
-                    $output->writeln("{$results['saved']} saved, {$results['skipped']} skipped");
+                    $this->successNotification($results, $url, $referer);
                 } else {
-                    $output->writeln("Failed to scan {$url} (Referer: {$referer})");
+                    $this->failureNotification($url, $referer);
                 }
                 $em->remove($queuedUrl);
             }
-            $em->flush();
         }
+        $em->flush();
 
         $lock->release();
         return 0;
@@ -80,24 +83,27 @@ class ScanCommand extends ContainerAwareCommand
     /**
      * @param string $url
      * @param string $referer
-     * @param OutputInterface $output
      */
-    private function scan($url, $referer, OutputInterface $output)
+    private function scan($url, $referer)
     {
+        $logger = $this->getContainer()->get('logger');
         $engine = $this->getContainer()->getParameter('scanner_engine');
         $appRoot = $this->getContainer()->getParameter('kernel.root_dir') . '/..';
 
-        $pathes = [
-            'SLIMERJS_EXECUTABLE'  => "{$appRoot}/scanner/node_modules/.bin/slimerjs",
-            'PHANTOMJS_EXECUTABLE' => "{$appRoot}/scanner/node_modules/casperjs/node_modules/.bin/phantomjs",
+        $paths = [
+            'SLIMERJS_EXECUTABLE'  => $this->getPath('slimerjs_path', "{$appRoot}/scanner/node_modules/.bin/slimerjs"),
+            'PHANTOMJS_EXECUTABLE' => $this->getPath('phantomjs_path', "{$appRoot}/scanner/node_modules/casperjs/node_modules/.bin/phantomjs"),
         ];
+        foreach ($paths as $var => $path) {
+            $logger->debug("{$var} => {$path}");
+        }
 
         $processBuilder = new ProcessBuilder();
         if ($engine == 'slimerjs') {
-            //$processBuilder->add('xvfb-run');
+            $processBuilder->add('xvfb-run');
         }
         $processBuilder
-            ->addEnvironmentVariables($pathes)
+            ->addEnvironmentVariables($paths)
             ->add("{$appRoot}/scanner/node_modules/.bin/casperjs")
             ->add("--engine={$engine}")
             ->add("{$appRoot}/scanner/scanner.js")
@@ -105,6 +111,7 @@ class ScanCommand extends ContainerAwareCommand
             ->add($referer);
 
         $process = $processBuilder->getProcess();
+        $logger->debug($process->getCommandLine());
         $process->run();
 
 
@@ -120,11 +127,31 @@ class ScanCommand extends ContainerAwareCommand
             }
         );
 
+        $stderr = $process->getErrorOutput();
+        if ($stderr) {
+            foreach (explode("\n", $stderr) as $line) {
+                $logger->error($line);
+            }
+        }
+
         return json_decode(implode("\n", $output), JSON_OBJECT_AS_ARRAY);
+    }
+
+    private function getPath($varname, $default)
+    {
+        if (!isset($this->pathes[$varname])) {
+            try {
+                $this->pathes[$varname] = $this->getContainer()->getParameter($varname);
+            } catch (\Exception $e) {
+                $this->pathes[$varname] = $default;
+            }
+        }
+        return $this->pathes[$varname];
     }
 
     private function saveDownloads(array $downloads)
     {
+        $logger = $this->getContainer()->get('logger');
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $result = ['saved' => 0, 'skipped' => 0];
         foreach ($downloads as $download) {
@@ -146,12 +173,40 @@ class ScanCommand extends ContainerAwareCommand
                 }
                 $em->persist($downloadEntity);
                 $result['saved']++;
+                $logger->debug("{$download['url']} from {$download['referer']} saved");
             } else {
                 $result['skipped']++;
+                $logger->error("{$download['url']} from {$download['referer']} skipped");
             }
         }
         $em->flush();
         return $result;
+    }
+
+    private function successNotification($results, $url, $referer)
+    {
+        $notification = new Notification();
+        $notification
+            ->setType('info')
+            ->setTitle('Scan successful')
+            ->setText("{$results['saved']} saved, {$results['skipped']} skipped")
+            ->setUrl($url)
+            ->setReferer($referer);
+        $this->getContainer()->get('doctrine.orm.entity_manager')->persist($notification);
+        $this->getContainer()->get('logger')->info($notification->getText());
+    }
+
+    private function failureNotification($url, $referer)
+    {
+        $notification = new Notification();
+        $notification
+            ->setType('error')
+            ->setTitle('Scan failed')
+            ->setText("Failed to scan {$url}")
+            ->setUrl($url)
+            ->setReferer($referer);
+        $this->getContainer()->get('doctrine.orm.entity_manager')->persist($notification);
+        $this->getContainer()->get('logger')->error($notification->getText() . " (Referer: {$referer})");
     }
 
 
---
+++ src/AppBundle/Entity/Notification.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Notification
+ * @package AppBundle\Entity
+ * @ORM\Entity()
+ * @ORM\Table(name="notifications")
+ */
+class Notification implements \JsonSerializable
+{
+    /**
+     * @var integer
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text")
+     */
+    private $text = '';
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title = '';
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=10)
+     */
+    private $type = 'info';
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=1024)
+     */
+    private $url = '';
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=1024)
+     */
+    private $referer = '';
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     */
+    private $created = null;
+
+    /**
+     * Notification constructor.
+     */
+    public function __construct()
+    {
+        $this->created = new \DateTime();
+    }
+
+    function __toString()
+    {
+        return json_encode($this);
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    function jsonSerialize()
+    {
+        return [
+            'type'    => $this->type,
+            'title'   => $this->title,
+            'text'    => $this->text,
+            'url'     => $this->url,
+            'referer' => $this->referer,
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     * @return $this
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return $this
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReferer()
+    {
+        return $this->referer;
+    }
+
+    /**
+     * @param string $referer
+     * @return $this
+     */
+    public function setReferer($referer)
+    {
+        $this->referer = $referer;
+        return $this;
+    }
+
+}
